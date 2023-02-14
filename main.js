1ec68d1650ecdef8153181ba24473905bbdd2686
xloldd <119611835+xloldd@users.noreply.github.com>
2023-02-14 14:07:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-/*Don't mind this, just ignore.
-If you read this you're bad*/
+console.log("If you see this through console, just know that the SRC is public and you don't need to use the devtools");
+console.log("fuck")
+console.log("hi")
+console.log("lol")
